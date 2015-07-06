125231a462a83f52d1ef9778b00e7a40526a41f9
Eric Bailey <eric@ericb.me>
2015-07-06 14:28:01 -0500

    updated dependencies and cleaned up project.clj

--- project.clj
+++ project.clj
@@ -5,18 +5,20 @@
              "-Dcookie=nocookie"
              "-Depmd_port=15000"]
   :description "Clojure + LFE"
-  :url "http://vonmo.com/projects/cljnode"
+  :url "https://github.com/lfex/lfecljapp"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :resource-paths [ ]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.4"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+
+  :resource-paths []
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                  [org.erlang.otp/jinterface "1.5.6"]]
+                 [org.erlang.otp/jinterface "1.5.6"]]
   :main cljnode.core
   :source-paths ["src/clj"]
   :target-path "target/"
