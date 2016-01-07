3bf248d0ec1b2943cab915aaf4116fd4d95cc17b
Eric Bailey <eric@ericb.me>
2016-01-07 03:41:08 -0600

    Clean up project.clj and rebar.config

--- project.clj
+++ project.clj
@@ -1,25 +1,25 @@
 (defproject cljnode "0.3.0"
-  :jvm-opts ["-Ddev_env=dev"
-             "-Dnode=cljnode@127.0.0.1"
-             "-Dmbox=mboxname"
-             "-Dcookie=nocookie"
-             "-Depmd_port=15000"]
-  :description "Clojure + LFE"
-  :url "https://github.com/lfex/lfecljapp"
+  :jvm-opts       ["-Ddev_env=dev"
+                   "-Dnode=cljnode@127.0.0.1"
+                   "-Dmbox=mboxname"
+                   "-Dcookie=nocookie"
+                   "-Depmd_port=15000"]
+  :description    "Clojure + LFE"
+  :url            "https://github.com/lfex/lfecljapp"
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license        {:name "Eclipse Public License"
+                   :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :resource-paths []
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.13"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jmdk/jmxtools
-                                                    com.sun.jmx/jmxri]]
-                 [org.erlang.otp/jinterface "1.6"]]
-  :main cljnode.core
+  :dependencies   [[org.clojure/clojure "1.8.0-RC4"]
+                   [org.clojure/tools.logging "0.3.1"]
+                   [org.slf4j/slf4j-log4j12 "1.7.13"]
+                   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                      javax.jms/jms
+                                                      com.sun.jmdk/jmxtools
+                                                      com.sun.jmx/jmxri]]
+                   [org.erlang.otp/jinterface "1.5.6"]]
+
+  :main         cljnode.core
   :source-paths ["src/clj"]
-  :target-path "target/"
-  :profiles {:uberjar {:aot :all}})
+  :profiles     {:uberjar {:aot :all}})
