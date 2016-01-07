d61ac7a188e83c17d5f125082b3dd83a561342ef
Eric Bailey <eric@ericb.me>
2016-01-07 02:57:18 -0600

    Upgrade Clojure dependencies

--- project.clj
+++ project.clj
@@ -11,14 +11,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :resource-paths []
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [org.slf4j/slf4j-log4j12 "1.7.13"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                 [org.erlang.otp/jinterface "1.5.6"]]
+                 [org.erlang.otp/jinterface "1.6"]]
   :main cljnode.core
   :source-paths ["src/clj"]
   :target-path "target/"
